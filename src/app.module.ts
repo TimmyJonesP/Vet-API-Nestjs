2aa4e8ea30cb4c9782e2a0f3b1ac96d338c39581
Sandro <sandrocarioli43@gmail.com>
2024-04-15 18:25:51 -0300

    render ready

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,34 @@
 import { Module } from "@nestjs/common";
-import { CatsModule } from "./cats/cats.module";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "./auth/auth.module";
 import { BreedsModule } from "./breeds/breeds.module";
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { CatsModule } from "./cats/cats.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3308,
-      username: "sandrocarioli43@gmail.com",
-      password: "root",
-      database: "db_crud",
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: process.env.POSTGRES_SSL === "true",
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === "true"
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
     }),
     CatsModule,
     BreedsModule,
